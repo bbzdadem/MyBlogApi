b5e8821eac6d7f716e88740c315daa2e1c610ca3
Adam Baba <bbzdadem@gmail.com>
2021-01-02 15:29:43 +0400

    first commit

---
+++ api/models/posts.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const PATH = "./data.json";
+
+class Post {
+    get(){
+        return this.readData();
+    }
+
+    getIndividualBlog(postId) {
+        const posts = this.readData();
+        const foundPost = posts.find((post) => post.id == postId)
+        return foundPost;
+    }
+
+
+    add(newPost){
+        const currentPosts = this.readData();
+        currentPosts.unshift(newPost);
+        this.storeData(currentPosts);
+    }
+
+    readData() {
+        let rawData = fs.readFileSync(PATH);
+        let posts = JSON.parse(rawData);
+        return posts;
+    }
+
+    storeData(rawData) {
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
+    }
+}
+
+module.exports = Post;
